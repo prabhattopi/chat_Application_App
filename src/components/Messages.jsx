ff5ce43674327be0947a379a11f0b994b5d6caa6
prabhattopi <topi9864@gmail.com>
2022-09-19 00:34:43 +0530

    The file

---
+++ src/components/Messages.jsx
@@ -0,0 +1,33 @@
+import { doc, onSnapshot } from 'firebase/firestore'
+import React, { useContext, useEffect, useRef } from 'react'
+import { useState } from 'react'
+import { ChatContext } from '../context/ChatContext'
+import { db } from '../firebase'
+import Message from './Message'
+
+const Messages = () => {
+  const [messages,setMessages]=useState([])
+  const {data}=useContext(ChatContext)
+
+
+  useEffect(() => {
+    const unsub=onSnapshot(doc(db,"chats",data.chatId),(doc)=>{
+      doc.exists() && setMessages(doc.data().messages)
+    })
+    return () => {
+     unsub()
+    }
+  }, [data.chatId])
+
+  return (
+    <div className='messages'>
+      {messages.map(m=>(
+ <Message message={m} key={m.id}/>
+      ))}
+       
+      
+    </div>
+  )
+}
+
+export default Messages
---
+++ src/pages/Login.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import Add from "../img/addAvatar.png";
+import { auth } from "../firebase";
+const LoginPages = () => {
+
+  const [errrr,setErr]=useState(false)
+  const navigate=useNavigate()
+  const handleSubmit=async(e)=>{
+    e.preventDefault()
+
+    const email=e.target[0].value;
+    const password=e.target[1].value;
+
+
+
+    try{
+     await signInWithEmailAndPassword(auth, email, password)
+     navigate("/")
+
+    }
+    catch(err){
+    setErr(true)
+
+    }
+
+
+
+
+
+  }
+
+
+
+
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Prabhat ki Gossips</span>
+        <span className="title">Login</span>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+
+          <button type="submit">Login Up</button>
+          {errrr&&<span>Something went Wrong</span>}
+        </form>
+        <p>You don't have an account ?<Link to="/register">Register</Link></p>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPages;
